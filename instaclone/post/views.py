e5268f6b6b61a251ac66119cc8848d8f90a43592
wrkbr <zcarc1@gmail.com>
2020-02-20 11:18:42 +0900

    30. 좋아요 북마크 view ajax

--- instaclone/post/views.py
+++ instaclone/post/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+import json
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -96,3 +99,46 @@ def post_delete(request, pk):
         post.delete()
         messages.success(request, '삭제완료')
         return redirect('post:post_list')
+
+
+@login_required # 로그인 되어있을 경우만 받습니다.
+@require_POST # POST 방식으로만 내용을 받습니다.
+# request에 ajax 통신으로 보낸 pk 값이 들어있습니다.
+def post_like(request):
+    pk = request.POST.get('pk', None)
+    post = get_object_or_404(Post, pk=pk)
+
+    # like 중계 모델을 사용해서 사용자 끼리 일종의 스위치를 만드는 것입니다.
+    post_like, post_like_created = post.like_set.get_or_create(user=request.user)
+
+    # like가 만들어지지 않은 상태라면
+    if not post_like_created:
+        post_like.delete()
+        message = "좋아요 취소"
+    else:
+        message = "좋아요"
+
+    context = {'like_count': post.like_count,
+               'message:': message}
+
+    return HttpResponse(json.dumps(context), content_type="application/json")
+
+
+def post_bookmark(request):
+    pk = request.POST.get('pk', None)
+    post = get_object_or_404(Post, pk=pk)
+
+    # like 중계 모델을 사용해서 사용자 끼리 일종의 스위치를 만드는 것입니다.
+    post_bookmark, post_bookmark_created = post.bookmark_set.get_or_create(user=request.user)
+
+    # like가 만들어지지 않은 상태라면
+    if not post_bookmark_created:
+        post_bookmark.delete()
+        message = "북마크 취소"
+    else:
+        message = "북마크"
+
+    context = {'bookmark_count': post.bookmark_count,
+               'message:': message}
+
+    return HttpResponse(json.dumps(context), content_type="application/json")
