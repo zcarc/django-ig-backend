9b47da29e5a67f4433a87d669f8486ee0041b373
wrkbr <zcarc1@gmail.com>
2020-02-20 15:13:54 +0900

    32. 댓글 model admin

--- instaclone/post/models.py
+++ instaclone/post/models.py
@@ -94,3 +94,15 @@ class Bookmark(models.Model):
         )
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.CharField(max_length=40)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.content
